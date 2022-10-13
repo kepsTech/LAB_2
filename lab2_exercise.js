b70b12b94c79249f8b622858ad0c6cfca23f57ea
kepsTech <afolabionikepo0@gmail.com>
2022-10-13 13:59:17 +0300

    new

--- lab2_exercise.js
+++ lab2_exercise.js
@@ -2,4 +2,4 @@ The student is able to write solution algorithms
 for the given problem with the chosen programming language. 
 The student is able to identify the typical phases and activities in the software development Process
 The student knows the high level difference of the most common processes
-The stydent recognizes the need and use of different tools during software development.
+The stydent recognizes the need and use of different tools during software development. Line5
